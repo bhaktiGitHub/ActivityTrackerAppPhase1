84a6e4139ccad5a102c3b2a36703262f9b414668
Bhakti Wakchaure <bhakti.wakchaure@gmail.com>
2018-04-14 01:33:45 -0700

    added new files  test

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
 
+app.get('/activity' , (req,res) =>
+{
+	console.log("inside get activity");
+	res.sendFile(__dirname + '/public/activity.html');
+});
+
 module.exports = app;
